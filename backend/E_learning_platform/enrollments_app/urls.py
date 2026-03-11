fc51e1b6d016b78988fd3d38fc0e0acf3adac7f2
barakabahoALINE <barakabahoaline@gmail.com>
2026-03-11 15:05:49 +0200

    student enrollment apis

--- backend/E_learning_platform/enrollments_app/urls.py
+++ backend/E_learning_platform/enrollments_app/urls.py
@@ -3,10 +3,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-
     path("enrollments/", AdminListEnrollmentsView.as_view()),
-    path("enrollments/<int:pk>/", AdminEnrollmentDetailView.as_view()),
+    path("enrollments/admin/<int:pk>/", AdminListEnrollmentsView.as_view()),
     path("enrollments/<int:pk>/update/", AdminUpdateEnrollmentView.as_view()),
     path("enrollments/<int:pk>/delete/", AdminDeleteEnrollmentView.as_view()),
     path("enrollments/create/", AdminCreateEnrollmentView.as_view()),
+    path("enroll/",EnrollCourseAPIView.as_view(),name="enroll-course",), # 1️⃣ Enroll in a course
+    path("my-courses/",MyEnrollmentsAPIView.as_view(),name="my-enrollments",), # 2️⃣ List my enrollments
+    path("enrollments/<int:pk>/",EnrollmentDetailAPIView.as_view(),name="enrollment-detail",),# 3️⃣ Retrieve single enrollment
+    path("enrollments/<int:pk>/cancel/",CancelEnrollmentAPIView.as_view(),name="cancel-enrollment",), # 4️⃣ Cancel enrollment
+   
 ]
